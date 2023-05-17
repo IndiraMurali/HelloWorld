463ff1720845c3546669e918b678b6e3ee8cf52b
Indira Muralidharan <133755901+IndiraMurali@users.noreply.github.com>
2023-05-17 12:47:21 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,11 @@
 def hello():
     return f"This is my first Hello world program"
 
+def hi():
+    return f"Inside Hi Function"
+
 if __name__ == "__main__":
     print("Inside Main Function")
     obj = hello()
+    hi()
     print(obj)
